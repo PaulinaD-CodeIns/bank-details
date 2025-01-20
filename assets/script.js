8e0c79a1ebb20181eff81f5a39e8a87ad9678799
Paulina Dolder <o4bhm9f4@students.codeinstitute.net>
2025-01-20 15:00:57 +0000

    Another attempt at debugging

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
 window.onload = function() {
     console.log("Welcome!");
 };
-    
 
+const startButton = document.getElementById('start-button');
+startButton.addEventListener('onclick', StartQuiz); 
+    
+function StartQuiz() {
+    console.log('Game Started');
+    startButton.classList.add('hide');
+}
+
+
+function SetNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
