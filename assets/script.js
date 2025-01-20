e249ee1e645622f45db4b8c45946da7d2353f686
Paulina Dolder <o4bhm9f4@students.codeinstitute.net>
2025-01-20 15:39:45 +0000

    Fixed filepath issues, inital JS put in place, ID's rretrieved, start function created and checked.

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,41 @@
 window.onload = function() {
-    console.log("Welcome!");
+    console.log("Welcome to the Quiz!");
 };
 
-const startButton = document.getElementById('start-button');
-startButton.addEventListener('onclick', StartQuiz); 
-    
-function StartQuiz() {
-    console.log('Game Started');
-    startButton.classList.add('hide');
-}
 
+let startButton = document.getElementById('start-button');
+let quizContainer = document.getElementById('quiz-container');
+let questions = document.getElementById('questions');
+let answerButtons = document.getElementById('answer-buttons');
+let nextButton = document.getElementById('next-button');
+
+    startButton.addEventListener('click', StartQuiz);
+
+
+function StartQuiz() {
+    console.log("Game Started!");
+    let username 
+    do {
+        username = prompt("Welcome to the Quiz! Please enter your username:");
+        
+        if (username === null) {
+            alert("You must enter a username to continue, sorry!");
+        }
+    } while (!username || username.trim() === "");
+    
+    console.log("Username: " + username);
+    localStorage.setItem("username", username);
+    
+
+
+    startButton.classList.add('hide');
+    quizContainer.classList.remove('hide');
+    questions.classList.remove('hide');
+    answerButtons.classList.remove('hide');
+    nextButton.classList.remove('hide');
+
+    SetNextQuestion();
+}
 
 function SetNextQuestion() {
 
